605cb07837ca0ecd9c58b92cf0a7ee4de4359c3b
Rinat Ziyayev <rinatziyayev@gmail.com>
2020-04-23 10:50:24 -0400

    attack4

--- attack.c
+++ attack.c
@@ -4,5 +4,5 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-    system("/usr/local/bin/score acc10507-c94e-421f-bf7e-6c40ae4c1609o");
+    system("/usr/local/bin/score acc10507-c94e-421f-bf7e-6c40ae4c1609");
 }
